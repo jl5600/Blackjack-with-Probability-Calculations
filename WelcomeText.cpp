60cdb7a330d0bc22b0dd2367c019bd2482c42f60
jl5600 <jason@lemosfamily.net>
2024-12-12 18:13:43 -0500

    Add files via upload

---
+++ WelcomeText.cpp
@@ -0,0 +1,10 @@
+#include "BlackjackProb.h"
+using namespace std;
+
+void WelcomeText() {
+
+	cout << fixed << showpoint;
+	cout << "BlackJack Probabilities" << endl;
+	cout << "Press S to start, or Q to quit." << endl;
+
+}
